552151703565b1c3f7ccf99e4828e16929cee640
AdomsNavicki <adomsnavicki@gmail.com>
2023-03-14 00:21:28 +0200

    form-add

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -1,10 +1,72 @@
 import React from "react";
+import { useState } from "react";
+import "../Scss/signUpForm.scss";
+import SignUpLogo from "../Svg/SignUpLogo.svg";
+
 import "../Scss/button.scss";
 const Button = ({ text }) => {
+  const DataObj = {
+    name: "Jonas",
+    username: "Baronas",
+    email: "15",
+    phone: "45646546",
+    website: "",
+    profession: "",
+  };
+
+  const [form, setForm] = useState([]);
+  const signUpHandler = () => {
+    setForm([DataObj]);
+  };
+
   return (
-    <div className="button-wrapper">
-      <button className="button button-font-link">{text}</button>
-    </div>
+    <>
+      <div className="button-wrapper">
+        <button onClick={signUpHandler} className="button button-font-link">
+          {text}
+        </button>
+      </div>
+
+      {form &&
+        form.length > 0 &&
+        form.map((formData, index) => (
+          <div key={index}>
+            <form className="register-form">
+              <button className="close-form-button">X</button>
+              <img
+                className="sign-up-logo"
+                src={SignUpLogo}
+                alt="form-logo"
+              ></img>
+              <div className="form-control">
+                <label className="contact-name">Name:</label>
+                <input className="text-input" type="text"></input>
+              </div>
+              <div className="form-control">
+                <label className="contact-name">Username:</label>
+                <input className="text-input" type="text"></input>
+              </div>
+              <div className="form-control">
+                <label className="contact-name">Email:</label>
+                <input className="text-input" type="text"></input>
+              </div>
+              <div className="form-control">
+                <label className="contact-name">Phone:</label>
+                <input className="text-input" type="text"></input>
+              </div>
+              <div className="form-control">
+                <label className="contact-name">Website:</label>
+                <input className="text-input" type="text"></input>
+              </div>
+              <div className="form-control">
+                <label className="contact-name">Profession:</label>
+                <input className="text-input" type="text"></input>
+              </div>
+              <input className="create-button" type="submit" />
+            </form>
+          </div>
+        ))}
+    </>
   );
 };
 
